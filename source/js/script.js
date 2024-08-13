ee199bbf6da57fd97bba03e29aa9db5d4d19f468
Tastan <tastanbay02@gmail.com>
2024-08-13 21:01:55 +0500

    add js

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,27 @@
+const buttonOpened = document.querySelector('.main-nav__burger');
+const menu = document.querySelector('.main-nav__menu');
+const buttonClosed = document.querySelector('.main-nav__close');
 
+const closeMenu = () => {
+    menu.classList.remove('main-nav__menu--showed');  
+    buttonClosed.removeEventListener('click', closeMenu);
+};
+
+buttonOpened.addEventListener('click', function() {
+    menu.classList.add('main-nav__menu--showed');
+    buttonClosed.addEventListener('click', closeMenu);
+});
+
+
+const buttonClick = document.querySelectorAll('.product__click');
+
+
+buttonClick.forEach(function(button) { 
+    button.addEventListener('click', function() { 
+        document.querySelector('.cart').style.display = 'block'; 
+    });
+});
+
+document.querySelector('.cart__close').addEventListener('click', function() {
+    document.querySelector('.cart').style.display = 'none';
+});
